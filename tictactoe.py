99709b990abcc82037a61cc26ff88086be02f9da
dansman805 <dansman805@gmail.com>
2017-08-31 17:11:00 -0400

    Added saving of game tree, still no loading.

--- tictactoe.py
+++ tictactoe.py
@@ -1,21 +1,22 @@
 import Tkinter as tk
 from PIL import Image, ImageTk
 from random import randint
+import cPickle as pickle
 
 EMPTY = 0
 CROSS = 1
 NOUGHT = 2
-PLAYER_NAMES = ['Nobody', 'Computer', 'Player']    
+PLAYER_NAMES = ['Nobody', 'Computer', 'Player']
 
 class Board:
 	WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-	
+
 	def __init__(self, cells = [EMPTY]*9):
 		self.cells = cells
 		self.nextMove = NOUGHT
 		self.rank = 0
 		self.children = {}
-		
+
 	def isWon(self):
 		for combo in Board.WINNING_COMBOS:
 			val = self.cells[combo[0]] * self.cells[combo[1]] * self.cells[combo[2]]
@@ -23,8 +24,8 @@ class Board:
 			if (val == NOUGHT**3): return NOUGHT
 		return EMPTY
 	def cell(self, x,y): return self.cells[y*3+x]
-		
-		
+
+
 	# creates a new board after "nextMove" party puts a mark at the cell "index"
 	def move(self, index):
 		if self.cells[index] != EMPTY: return None # the cell is already taken
@@ -32,31 +33,34 @@ class Board:
 		result.cells[index] = self.nextMove
 		result.nextMove = NOUGHT if self.nextMove == CROSS else CROSS
 		return result
-		
+
 def displayMove(index, value):
 	buttons[index].image = images[value]
 	buttons[index].configure(image=images[value])
 	buttons[index].config(state='disabled')
-	
+
+file_name = 'tree.pkl'
+
 # builds game tree; assumes "board" does NOT represent a winning position
 def buildGameTree(board):
+	treeReadFile = open(file_name, 'r')
 	won = board.isWon()
 	if won == CROSS: board.rank = 1; return
 	if won == NOUGHT: board.rank = -1; return
-	
+
 	for index in range(0,9):
 		child = board.move(index)
 		if child != None:
 			buildGameTree(child)
 			board.children[index] = child
-			
+
 	if not board.children: board.rank = 0; return
-	
+
 	childRanks = [child.rank for child in board.children.values()]
-	
-	if board.nextMove == CROSS: board.rank = max(childRanks)	
+
+	if board.nextMove == CROSS: board.rank = max(childRanks)
 	elif board.nextMove == NOUGHT: board.rank = min(childRanks)
-		
+
 currentBoard = Board()
 buttons = []
 
@@ -69,10 +73,10 @@ images = [ loadImage('Empty Square'), loadImage('X Square'), loadImage('O Square
 
 def getPlayerName(side):
 	return PLAYER_NAMES[side]
-	
+
 def disableAllbuttons():
 	for x in buttons:
-		x.config(state = 'disabled')  
+		x.config(state = 'disabled')
 
 
 
@@ -89,7 +93,7 @@ def isEmpty(value):
 		return currentBoard.cell(1,0) == EMPTY or currentBoard.cell(0,1) == EMPTY or currentBoard.cell(2,1) == EMPTY or currentBoard.cell(1,2) == EMPTY
 	elif value == 'center':
 		return currentBoard.cell(1,1) == EMPTY
-		
+
 def machineMove():
 	global currentBoard
 	if not currentBoard.children: announceWinner(); return
@@ -97,7 +101,7 @@ def machineMove():
 	currentBoard = currentBoard.children[index]
 	displayMove(index, CROSS)
 	if currentBoard.isWon(): announceWinner()
-	
+
 def onButtonPress(button, x,y):
 	global currentBoard
 	index = y*3+x
@@ -160,11 +164,16 @@ def createbuttons():
 	BR.config(image=imageBR)
 	BR.image = imageBR
 	BR.grid(column=2, row=2)
-	
+
 	return [TL, TM, TR, ML, MM, MR, BL, BM, BR]
 
 buttons = createbuttons()
 
+buildGameTree(currentBoard)
+
+# open the file for writing
+fileObject = open(file_name,'wb')
+pickle.dump(currentBoard.children, fileObject)
+
 if __name__ == '__main__':
-	buildGameTree(currentBoard)
 	window.mainloop()
